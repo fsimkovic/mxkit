8192cb9de7cf628d101280023321e04d2841bc5f
Felix Simkovic <felixsimkovic@me.com>
2017-02-20 11:36:44 +0000

    added function to check for executable and added TMscore binary example

---
+++ mrkit/cli/TMscore.py
@@ -0,0 +1,87 @@
+"""Python wrapper module for the TMscore binary"""
+
+__author__ = "Felix Simkovic"
+__date__ = "20 Feb 2017"
+__version__ = 0.1
+
+from mrkit.cli import AbstractCommandline
+from mrkit.cli import Argument
+from mrkit.cli import Option
+from mrkit.cli import Switch
+
+
+class TMscoreCommandline(AbstractCommandline):
+    """Python wrapper for the TMscore [#]_ binary
+
+    Description
+    -----------
+    TM-score [#]_ is a metric for measuring the structural similarity of two protein models.
+    It is designed to solve two major problems in the traditional metrics such as
+    root-mean-square deviation (RMSD): (1) TM-score measures the global fold similarity
+    and is less sensitive to the local structural variations; (2) magnitude of TM-score
+    for random structure pairs is length-independent. TM-score has the value in (0,1],
+    where 1 indicates a perfect match between two structures. Following strict statistics
+    of structures in the PDB, scores below 0.17 corresponds to randomly chosen unrelated
+    proteins whereas with a score higher than 0.5 assume generally the same fold in SCOP/CATH.
+
+    .. [#] Zhang Y, Skolnick J. (2004). Scoring function for automated assessment
+       of protein structure template quality, Proteins, 57: 702-710.
+
+    Examples
+    --------
+
+    1. Run TM-score to compare 'model' and 'native':
+    >>> from mrkit.cli import TMscore
+    >>> tm_exe = TMscore.TMscoreCommandLine(
+    ...     "/usr/bin/TMscore",
+    ...     model="model.pdb", native="native.pdb")
+    >>> print(tm_exe)
+    /usr/bin/TMscore model.pdb native.pdb
+
+     2. Run TM-score to compare two complex structures with multiple chains
+     >>> from mrkit.cli import TMscore
+     >>> tm_exe = TMscore.TMscoreCommandLine(
+     ...     "/usr/bin/TMscore", complex=True,
+     ...     model="model.pdb", native="native.pdb")
+     >>> print(tm_exe)
+    /usr/bin/TMscore -c model.pdb native.pdb
+
+     3. TM-score normalized with an assigned scale d0 e.g. 5 A
+     >>> from mrkit.cli import TMscore
+     >>> tm_exe = TMscore.TMscoreCommandLine(
+     ...     "/usr/bin/TMscore", norm_scale=5,
+     ...     model="model.pdb", native="native.pdb")
+     >>> print(tm_exe)
+     /usr/bin/TMscore model native -d 5
+
+     4. TM-score normalized by a specific length, e.g. 120 AA
+     >>> from mrkit.cli import TMscore
+     >>> tm_exe = TMscore.TMscoreCommandLine(
+     ...     "/usr/bin/TMscore", norm_length=5,
+     ...     model="model.pdb", native="native.pdb")
+     >>> print(tm_exe)
+     /usr/bin/TMscore model native -l 120
+
+    """
+    
+    def __init__(self, cmd='TMscore', **kwargs):
+        self.parameters = [
+            Switch(['-c', 'complex'],
+                   'Run TM-score to compare two complex structures with multiple chains'),
+            Option(['-d', 'norm_scale'],
+                   'TM-score normalized with an assigned scale d0',
+                    equate=False),
+            Option(['-l', 'norm_length'],
+                   'TM-score normalized by a specific length',
+                   equate=False),
+
+            Argument(['model'],
+                     "Input model structure",
+                     filename=True,
+                     is_required=True),
+            Argument(['native'],
+                      "Input native structure",
+                      filename=True,
+                      is_required=True),
+        ]
+        AbstractCommandline.__init__(self, cmd, **kwargs)
--- mrkit/cli/__init__.py
+++ mrkit/cli/__init__.py
@@ -18,13 +18,55 @@ from Bio.Application import _ArgumentList
 from Bio.Application import _Option
 from Bio.Application import _Switch
 
+import os
+
 
 class AbstractCommandline(AbstractCommandline):
     """Extension to the original :obj:`AbstractCommandline <Bio.Application.AbstractCommandline>`"""
 
+    def __init__(self, cmd, **kwargs):
+        """Initialise a new :obj:`AbstractCommandline`"""
+        # Check if the executable is available
+        cmd = AbstractCommandline.find_exec(cmd)
+        super(AbstractCommandline, self).__init__(cmd, **kwargs)
+
     def __call__(self, *args, **kwargs):
         """Overwrite parent __call__"""
         raise AttributeError("Execution of AbstractCommandline made unavailable")
+
+    @staticmethod
+    def find_exec(program, dirs=None):
+        """Find the executable exename.
+
+        Parameters
+        ----------
+        program : str
+           The name or path to an executable
+        dirs : list, tuple, optional
+           Additional directories to search for the location
+
+        """
+        def is_exe(exe):
+            return os.path.isfile(exe) and os.access(exe, os.X_OK)
+
+        fpath, fname = os.path.split(program)
+        if fpath:
+            if is_exe(program):
+                return program
+        else:
+            paths = os.environ["PATH"].split(os.pathsep)
+            if dirs:
+                # Convert in case it's a tuple
+                paths += list(dirs)
+
+            for path in paths:
+                path = path.strip('"')
+                exe_file = os.path.join(path, program)
+                if is_exe(exe_file):
+                    return exe_file
+
+        msg = "Executable unavailable: {0}".format(program)
+        raise ValueError(msg)
 
 
 class Argument(_Argument):
