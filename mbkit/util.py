316fa5440325edba47bb1b424754d7b540c87624
Felix Simkovic <felixsimkovic@me.com>
2017-05-24 16:56:33 +0100

    delete flag added to tmp_file

--- mbkit/util.py
+++ mbkit/util.py
@@ -3,11 +3,13 @@
 import tempfile
 
 
-def tmp_fname(directory=None, prefix='tmp', suffix=""):
+def tmp_fname(delete=False, directory=None, prefix='tmp', suffix=""):
     """Return a filename for a temporary file
 
     Parameters
     ----------
+    delete : bool, optional
+       Delete the file, thus return name only [default: True]
     directory : str, optional
        Path to a directory to write the files to.
     prefix : str, optional
@@ -16,7 +18,7 @@ def tmp_fname(directory=None, prefix='tmp', suffix=""):
        A suffix to the temporary filename
 
     """
-    tmpf = tempfile.NamedTemporaryFile(dir=directory, prefix=prefix, suffix=suffix, delete=False)
+    tmpf = tempfile.NamedTemporaryFile(delete=False, dir=directory, prefix=prefix, suffix=suffix)
     tmpf.close()
     return tmpf.name
 
