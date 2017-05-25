7fe486292a0cbe2e9c7856d0e405b1a8196ea3d6
Felix Simkovic <felixsimkovic@me.com>
2017-05-25 21:10:57 +0100

    bug fixes for SGE

--- mbkit/dispatch/sge.py
+++ mbkit/dispatch/sge.py
@@ -10,8 +10,9 @@ import os
 import re
 import shutil
 
-from mbkit.apps import SCRIPT_EXT
+from mbkit.apps import make_script, SCRIPT_EXT
 from mbkit.dispatch.cexectools import cexec
+from mbkit.util import tmp_fname
 
 logger = logging.getLogger(__name__)
 
@@ -119,7 +120,7 @@ class SunGridEngine(object):
 
     @staticmethod
     def qsub(command, deps=None, directory=None, hold=False, log=None, name=None, pe_opts=None,
-             priority=None, queue=None, shell=None, time=None, *args, **kwargs):
+             priority=None, queue=None, runtime=None, shell="/bin/bash", *args, **kwargs):
         """Submit a job to the SGE queue
 
         Parameters
@@ -142,10 +143,10 @@ class SunGridEngine(object):
            The priority level of the job
         queue : str, optional
            The queue to submit the job to
-        shell : str, optional
-           The absolute path to the shell to run the job in
-        time : int, optional
+        runtime : int, optional
            The maximum runtime of the job in seconds
+        shell : str, optional
+           The absolute path to the shell to run the job in [default: bash]
 
         """
         # Prepare the command with default options
@@ -154,31 +155,17 @@ class SunGridEngine(object):
         if len(command) > 1:
             if directory is None:
                 directory = os.getcwd()
-            # Write all jobs into an array.jobs file
-            array_jobs = os.path.join(directory, 'array.jobs')
-            with open(array_jobs, 'w') as f_out:
-                f_out.write(os.linesep.join(command))
-            # Create the actual executable script
-            array_script = os.path.join(directory, "array.script")
-            with open(array_script, 'w') as f_out:
-                # TODO: use MbKit make_script
-                f_out.write(os.linesep.join([
-                    "#!/bin/sh", 
-                    "script=`sed -n \"${{SGE_TASK_ID}}p\" {0}`".format(array_jobs),
-                    "log=\"${script%.*}\".log",
-                    "$script > $log 2>&1",
-                ]))
+            array_script, array_jobs = SunGridEngine._prep_array(command, directory)
             # Add command-line flags
             cmd += ["-t", "1-{0}".format(len(command)), "-tc", "{0}".format(len(command))]
-            # Redirect the log file and reset if provided
-            cmd += ["-j", "y", "-o", "/dev/null"]
-            log = None
-            # Overwrite the command
+            # Overwrite some defaults
             command = [array_script]
+            log = os.devnull
             # Save status
             array = True
         else:
             array = False
+        # Set the remaining options
         if deps:
             cmd += ["-hold_jid", "{0}".format(",".join(map(str, deps)))]
         if hold:
@@ -193,10 +180,10 @@ class SunGridEngine(object):
             cmd += ["-p", str(priority)]
         if queue:
             cmd += ["-q", queue]
+        if runtime:
+            cmd += ["-l", "h_rt={0}".format(runtime)]
         if shell:
             cmd += ["-S", shell]
-        if time:
-            cmd += ["-l", "h_rt={0}".format(time)]
         cmd += map(str, command)
         # Submit the job
         stdout = cexec(cmd, directory=directory)
@@ -204,3 +191,19 @@ class SunGridEngine(object):
         jobid = int(stdout.split()[2].split(".")[0]) if array else int(stdout.split()[2])
         logger.debug("Job %d successfully submitted to the SGE queue", jobid)
         return jobid
+
+    @staticmethod
+    def _prep_array(commands, directory):
+        """Prepare multiple jobs to be an array"""
+        # Write all jobs into an array.jobs file
+        array_jobs = tmp_fname(directory=directory, suffix='.jobs')
+        with open(array_jobs, 'w') as f_out:
+            f_out.write(os.linesep.join(commands) + os.linesep)
+        # Create the actual executable script
+        array_script = make_script(
+            [["script=`sed -n \"${{SGE_TASK_ID}}p\" {0}`".format(array_jobs)],
+             ["log=\"${script%.*}\".log"],
+             ["$script", ">", "$log", "2>&1"]],
+            directory=directory, suffix=".script"
+        )
+        return array_script, array_jobs
