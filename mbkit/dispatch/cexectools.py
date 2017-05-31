f0afa99f872d3ad68a0e7509c3cada71a84c0e3d
Felix Simkovic <felixsimkovic@me.com>
2017-05-31 10:44:48 +0100

    prep_array_scripts function generalized

--- mbkit/dispatch/cexectools.py
+++ mbkit/dispatch/cexectools.py
@@ -10,6 +10,8 @@ import signal
 import subprocess
 import sys
 
+from mbkit.util import tmp_fname
+
 logger = logging.getLogger(__name__)
 
 
@@ -40,8 +42,9 @@ def cexec(cmd, directory=None, stdin=None, permit_nonzero=False):
     """
     try:
         logger.debug("Executing '%s'", " ".join(cmd))
-        kwargs = {"bufsize":0, "shell":"False"} if os.name == "nt" else {}
-        p = subprocess.Popen(cmd, cwd=directory, stdin=subprocess.PIPE, stderr=subprocess.STDOUT, stdout=subprocess.PIPE, **kwargs)
+        kwargs = {"bufsize": 0, "shell": "False"} if os.name == "nt" else {}
+        p = subprocess.Popen(cmd, cwd=directory, stdin=subprocess.PIPE, stderr=subprocess.STDOUT,
+                             stdout=subprocess.PIPE, **kwargs)
         # We require the str.encode() and str.decode() functions for Python 2.x and 3.x compatibility
         stdout, _ = p.communicate(input=stdin.encode()) if stdin else p.communicate()
         stdout = stdout.decode()
@@ -51,9 +54,40 @@ def cexec(cmd, directory=None, stdin=None, permit_nonzero=False):
             logger.debug("Ignoring non-zero returncode %d for '%s'", p.returncode, " ".join(cmd))
             return stdout.strip()
         else:
-            msg = "Execution of '{0}' exited with non-zero return code ({1}): {2}" .format(' '.join(cmd), p.returncode, stdout)
+            msg = "Execution of '{0}' exited with non-zero return code ({1}): {2}" .format(' '.join(cmd),
+                                                                                           p.returncode, stdout)
             raise RuntimeError(msg)
     # Allow ctrl-c's
     except KeyboardInterrupt:
         os.kill(p.pid, signal.SIGTERM)
         sys.exit(signal.SIGTERM)
+
+
+def prep_array_scripts(scripts, directory, task_env):
+    """Prepare multiple jobs to be an array
+
+    Parameters
+    ----------
+    scripts : list
+       The scripts to be run as part of the array
+    directory : str
+       The directory to create the files in
+    task_env : str
+       The task environment variable
+
+    """
+    # Write all jobs into an array.jobs file
+    array_jobs = tmp_fname(directory=directory, prefix="array_", suffix='.jobs')
+    with open(array_jobs, 'w') as f_out:
+        f_out.write(os.linesep.join(scripts) + os.linesep)
+    # Create the actual executable script
+    array_script = array_jobs.replace(".jobs", ".script")
+    with open(array_script, "w") as f_out:
+        # Construct the content for the file
+        content = "#!/bin/sh{linesep}"
+        content += "script=`sed -n \"${{{task_env}}}p\" {array_jobs}`{linesep}"
+        content += "$script{linesep}"
+        content = content.format(array_jobs=array_jobs, linesep=os.linesep,
+                                 task_env=task_env)
+        f_out.write(content)
+    return array_script, array_jobs
--- mbkit/dispatch/tests/test_cexectools.py
+++ mbkit/dispatch/tests/test_cexectools.py
@@ -28,7 +28,7 @@ class Test(unittest.TestCase):
         self.assertEqual("hello", stdout)
     
     def test_cexec_4(self):
-        if (sys.version_info < (3, 0)):
+        if sys.version_info < (3, 0):
             cmd = [sys.executable, "-c", "import sys; print(raw_input()); sys.exit(0)"]
         else:
             cmd = [sys.executable, "-c", "import sys; print(input()); sys.exit(0)"]
@@ -40,6 +40,48 @@ class Test(unittest.TestCase):
         directory = os.path.join(os.getcwd(), 'mbkit', 'dispatch')
         stdout = mbkit.dispatch.cexectools.cexec(cmd, directory=directory)
         self.assertEqual(directory, stdout)
+
+    def test_prep_array_scripts_1(self):
+        scripts = ["/path/to/script1.sh", "/path/to/script2.sh"]
+        array_script, array_jobs = mbkit.dispatch.cexectools.prep_array_scripts(scripts, os.getcwd(), "SGE_TASK_ID")
+        array_jobs_content = [l.strip() for l in open(array_jobs).readlines()]
+        self.assertEqual(scripts, array_jobs_content)
+        array_script_content = [l.strip() for l in open(array_script).readlines()]
+        self.assertEqual(["#!/bin/sh", "script=`sed -n \"${{SGE_TASK_ID}}p\" {0}`".format(array_jobs), "$script"],
+                         array_script_content)
+        map(os.remove, [array_script, array_jobs])
+
+    def test_prep_array_scripts_2(self):
+        scripts = ["/path/to/script1.sh", "/path/to/script2.sh"]
+        array_script, array_jobs = mbkit.dispatch.cexectools.prep_array_scripts(scripts, os.getcwd(), "LSB_JOBINDEX")
+        array_jobs_content = [l.strip() for l in open(array_jobs).readlines()]
+        self.assertEqual(scripts, array_jobs_content)
+        array_script_content = [l.strip() for l in open(array_script).readlines()]
+        self.assertEqual(["#!/bin/sh", "script=`sed -n \"${{LSB_JOBINDEX}}p\" {0}`".format(array_jobs), "$script"],
+                         array_script_content)
+        map(os.remove, [array_script, array_jobs])
+
+    def test_prep_array_scripts_3(self):
+        scripts = ["/path/to/script1.sh", "/path/to/script2.sh"]
+        array_script, array_jobs = mbkit.dispatch.cexectools.prep_array_scripts(scripts, os.getcwd(), "RANDOM_TEXT")
+        array_jobs_content = [l.strip() for l in open(array_jobs).readlines()]
+        self.assertEqual(scripts, array_jobs_content)
+        array_script_content = [l.strip() for l in open(array_script).readlines()]
+        self.assertEqual(["#!/bin/sh", "script=`sed -n \"${{RANDOM_TEXT}}p\" {0}`".format(array_jobs), "$script"],
+                         array_script_content)
+        map(os.remove, [array_script, array_jobs])
+
+    def test_prep_array_scripts_4(self):
+        scripts = ["/path/to/script1.sh", "/path/to/script2.sh"]
+        array_script, array_jobs = mbkit.dispatch.cexectools.prep_array_scripts(scripts, "mbkit", "RANDOM_TEXT")
+        self.assertEqual(os.path.abspath("mbkit"), os.path.dirname(array_script))
+        self.assertEqual(os.path.abspath("mbkit"), os.path.dirname(array_jobs))
+        array_jobs_content = [l.strip() for l in open(array_jobs).readlines()]
+        self.assertEqual(scripts, array_jobs_content)
+        array_script_content = [l.strip() for l in open(array_script).readlines()]
+        self.assertEqual(["#!/bin/sh", "script=`sed -n \"${{RANDOM_TEXT}}p\" {0}`".format(array_jobs), "$script"],
+                         array_script_content)
+        map(os.remove, [array_script, array_jobs])
 
 
 if __name__ == "__main__":
